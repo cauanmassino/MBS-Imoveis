faf95a99bc2f500054b803b6f9bdfa11dcd5c74e
Cauan Massino <cauanmassino55@gmail.com>
2024-12-08 22:13:05 -0300

    setimo commit

---
+++ Js/scriptsemp.js
@@ -0,0 +1,16 @@
+let currentIndex = 0;
+const images = document.querySelectorAll('.carousel-image');
+const totalImages = images.length;
+
+function moveSlide(step) {
+    currentIndex += step;
+
+    if (currentIndex < 0) {
+        currentIndex = totalImages - 1; // Volta para a última imagem
+    } else if (currentIndex >= totalImages) {
+        currentIndex = 0; // Vai para a primeira imagem
+    }
+
+    const newTransformValue = -currentIndex * 100;
+    document.querySelector('.carousel-slide').style.transform = `translateX(${newTransformValue}%)`;
+}
